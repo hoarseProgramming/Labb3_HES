0373887924292a82f72ae66b66db9bf3f4a4f6bc
hoarseProgramming <hampus.es@gmail.com>
2024-10-23 21:33:17 +0200

    Add question model

---
+++ Model/Question.cs
@@ -0,0 +1,17 @@
+﻿namespace Labb3_HES.Model
+{
+    class Question
+    {
+        public Question(string query, string correctAnswer,
+            string incorrectAnswer1, string incorrectAnswer2, string incorrectAnswer3)
+        {
+            Query = query;
+            CorrectAnswer = correctAnswer;
+            IncorrectAnswers = [incorrectAnswer1, incorrectAnswer2, incorrectAnswer3];
+        }
+        public string Query { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string[] IncorrectAnswers { get; set; }
+
+    }
+}
