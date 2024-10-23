50bf3d2f5633b970e601b52a6fb47f356dbaaf83
hoarseProgramming <hampus.es@gmail.com>
2024-10-23 21:34:28 +0200

    Update basic functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Labb3_HES.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Labb3_HES
 {
@@ -19,6 +11,8 @@ namespace Labb3_HES
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
+
         }
     }
 }
