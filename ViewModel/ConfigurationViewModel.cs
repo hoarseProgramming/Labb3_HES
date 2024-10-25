41c086e98a3a3cf8e2597d772350853e9320fd10
hoarseProgramming <hampus.es@gmail.com>
2024-10-25 23:01:02 +0200

    Add active question property, and commands for adding and removing questions

--- ViewModel/ConfigurationViewModel.cs
+++ ViewModel/ConfigurationViewModel.cs
@@ -1,12 +1,53 @@
-﻿namespace Labb3_HES.ViewModel
+﻿using Labb3_HES.Command;
+using Labb3_HES.Model;
+
+namespace Labb3_HES.ViewModel
 {
     class ConfigurationViewModel : ViewModelBase
     {
         private readonly MainWindowViewModel? mainWindowViewModel;
 
+        public QuestionPackViewModel? ActivePack { get => mainWindowViewModel.ActivePack; }
+
+        public Question _activeQuestion;
+        public Question ActiveQuestion
+        {
+            get => _activeQuestion;
+            set
+            {
+                _activeQuestion = value;
+                RaisePropertyChanged();
+                RemoveQuestionCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public DelegateCommand AddQuestionCommand { get; }
+        public DelegateCommand RemoveQuestionCommand { get; }
+
         public ConfigurationViewModel(MainWindowViewModel? mainWindowViewModel)
         {
             this.mainWindowViewModel = mainWindowViewModel;
+            AddQuestionCommand = new DelegateCommand(AddQuestion);
+            RemoveQuestionCommand = new DelegateCommand(RemoveQuestion, CanRemoveQuestion);
         }
+
+        private bool CanRemoveQuestion(object? arg)
+        {
+            //TODO: return Active question != null;
+            return ActiveQuestion != null;
+        }
+
+        private void RemoveQuestion(object obj)
+        {
+            ActivePack.Questions.Remove(ActiveQuestion);
+        }
+
+        private void AddQuestion(object obj) => ActivePack.Questions.Add(new Question());
+
+        //private bool CanAddQuestion(object? arg)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+
     }
 }
