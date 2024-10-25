5ea27a34ae4a1ad9f0b4eb44f711a07a49399ac4
hoarseProgramming <hampus.es@gmail.com>
2024-10-25 22:59:19 +0200

    Add delegate command

---
+++ Command/DelegateCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace Labb3_HES.Command
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object?, bool> canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        //Raise Can ExecuteChanged when it's supposed to, will change the object
+        //that subscribes to this
+
+        public DelegateCommand(Action<object> execute, Func<object?, bool> canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public bool CanExecute(object? parameter) => canExecute is null ? true : canExecute(parameter);
+
+        public void Execute(object? parameter) => execute(parameter);
+
+    }
+}
