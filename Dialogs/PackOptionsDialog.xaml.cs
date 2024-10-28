8c4eb7c0aee994c7ed3338a1fde7ad463a49fec0
hoarseProgramming <hampus.es@gmail.com>
2024-10-28 16:55:58 +0100

    add datacontext

--- Dialogs/PackOptionsDialog.xaml.cs
+++ Dialogs/PackOptionsDialog.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Labb3_HES.Dialogs
 {
@@ -22,6 +10,7 @@ namespace Labb3_HES.Dialogs
         public PackOptionsDialog()
         {
             InitializeComponent();
+            DataContext = (App.Current.MainWindow as MainWindow).DataContext;
         }
     }
 }
