0d1aacc62721ebb94868d547f5831d0f6d76d610
hoarseProgramming <hampus.es@gmail.com>
2024-10-29 13:55:29 +0100

    add ViewCommands with command for opening packOptions

---
+++ Command/ViewCommands.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+
+namespace Labb3_HES.Command
+{
+    public class ViewCommands
+    {
+        public static RoutedUICommand OpenPackOptionsCommand
+                            = new RoutedUICommand("Open Pack Options", "OpenPackOptionsCommand", typeof(ViewCommands));
+    }
+}
