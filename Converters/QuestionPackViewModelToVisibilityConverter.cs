fce88e9cebe77492ec758fcdfafcf0883f5697bb
hoarseProgramming <hampus.es@gmail.com>
2024-10-29 13:54:44 +0100

    add questionPackViewModelToVisibilityConverter

---
+++ Converters/QuestionPackViewModelToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Labb3_HES.Converters
+{
+    class QuestionPackViewModelToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value == null ? Visibility.Hidden : Visibility.Visible;
+        //{
+        //    return value == null ? Visibility.Hidden : Visibility.Visible;
+        //    //return value switch
+        //    //{
+
+        //    //};
+        //}
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
