82d9fe0a62cf357ac858109dc66cb51965bee5fe
hoarseProgramming <hampus.es@gmail.com>
2024-10-30 13:34:02 +0100

    Add visibility and enableability

--- ViewModel/ConfigurationViewModel.cs
+++ ViewModel/ConfigurationViewModel.cs
@@ -7,9 +7,31 @@ namespace Labb3_HES.ViewModel
     {
         private readonly MainWindowViewModel? mainWindowViewModel;
 
+        private bool _isVisible;
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set
+            {
+                _isEnabled = value;
+                RaisePropertyChanged();
+            }
+        }
         public QuestionPackViewModel? ActivePack { get => mainWindowViewModel.ActivePack; }
 
         public Question _activeQuestion;
+
         public Question ActiveQuestion
         {
             get => _activeQuestion;
@@ -26,15 +48,23 @@ namespace Labb3_HES.ViewModel
         public ConfigurationViewModel(MainWindowViewModel? mainWindowViewModel)
         {
             this.mainWindowViewModel = mainWindowViewModel;
+            IsVisible = true;
+            IsEnabled = true;
             AddQuestionCommand = new DelegateCommand(AddQuestion);
             RemoveQuestionCommand = new DelegateCommand(RemoveQuestion, CanRemoveQuestion);
         }
 
         private bool CanRemoveQuestion(object? arg) => ActiveQuestion != null;
 
-        private void RemoveQuestion(object obj) => ActivePack.Questions.Remove(ActiveQuestion);
-
-        private void AddQuestion(object obj) => ActivePack.Questions.Add(new Question());
-
+        private void RemoveQuestion(object obj)
+        {
+            ActivePack.Questions.Remove(ActiveQuestion);
+            mainWindowViewModel.PlayerViewModel.PlayQuizCommand.RaiseCanExecuteChanged();
+        }
+        private void AddQuestion(object obj)
+        {
+            ActivePack.Questions.Add(new Question());
+            mainWindowViewModel.PlayerViewModel.PlayQuizCommand.RaiseCanExecuteChanged();
+        }
     }
 }
