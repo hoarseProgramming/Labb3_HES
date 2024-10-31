48420119f60a08ce63bff6da4a03bb50db244d5d
hoarseProgramming <hampus.es@gmail.com>
2024-10-31 09:45:44 +0100

    Reset after modifying

--- Command/ViewCommands.cs
+++ Command/ViewCommands.cs
@@ -4,10 +4,12 @@ namespace Labb3_HES.Command
 {
     public class ViewCommands
     {
+
         public static RoutedUICommand OpenPackOptionsCommand
                             = new RoutedUICommand("Open Pack Options", "OpenPackOptionsCommand", typeof(ViewCommands));
 
         public static RoutedUICommand CreateNewPackCommand
-                            = new RoutedUICommand("Create New pack", "CreatenewpackCommand", typeof(ViewCommands));
+                            = new RoutedUICommand("Create New pack", "CreateNewPackCommand", typeof(ViewCommands));
+
     }
 }
