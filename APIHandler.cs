d8dd0089f8a36fb3247501ef3c3b78fed0bf43a0
hoarseProgramming <hampus.es@gmail.com>
2024-11-05 10:12:34 +0100

    add APIhandler class for getting questions and categories from API

---
+++ APIHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+
+namespace Labb3_HES
+{
+    class APIHandler
+    {
+        public static async Task<CategoryList> GetQuestionCategories()
+        {
+            HttpClient client = new HttpClient();
+
+            //Asynchronous method starts
+            var serializedCategories = await client.GetStringAsync("https://opentdb.com/api_category.php");
+
+            var categoryList = JsonSerializer.Deserialize<CategoryList>(serializedCategories);
+
+            return categoryList;
+        }
+    }
+}
