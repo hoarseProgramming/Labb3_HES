81d42537ca3fb3ab53af264c030bec51ec8a3cea
hoarseProgramming <hampus.es@gmail.com>
2024-11-05 10:09:17 +0100

    add category class

---
+++ Category.cs
@@ -0,0 +1,15 @@
+﻿namespace Labb3_HES
+{
+    class Category
+    {
+        public Category(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int id { get; }
+        public string name { get; }
+
+    }
+}
