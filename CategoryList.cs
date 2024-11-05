f5fd17f4a048db8a4a54ca4c1eef711e1876a106
hoarseProgramming <hampus.es@gmail.com>
2024-11-05 10:09:26 +0100

    add category list class

---
+++ CategoryList.cs
@@ -0,0 +1,12 @@
+﻿namespace Labb3_HES
+{
+    class CategoryList
+
+    {
+        public List<Category> trivia_categories { get; }
+        public CategoryList(List<Category> trivia_categories)
+        {
+            this.trivia_categories = trivia_categories;
+        }
+    }
+}
