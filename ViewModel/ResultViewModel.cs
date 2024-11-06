5c9d359db20ed7f5418cf3a0a1124d3fbc53f9b2
hoarseProgramming <hampus.es@gmail.com>
2024-11-06 15:20:56 +0100

    add feedback messaging to player

--- ViewModel/ResultViewModel.cs
+++ ViewModel/ResultViewModel.cs
@@ -3,6 +3,7 @@
     internal class ResultViewModel : ViewModelBase
     {
         private readonly MainWindowViewModel? mainWindowViewModel;
+
         private bool _isResultMode;
         public bool IsResultMode
         {
@@ -33,6 +34,18 @@
                 RaisePropertyChanged();
             }
         }
+
+        private string _resultMessage;
+        public string ResultMessage
+        {
+            get => _resultMessage;
+            set
+            {
+                _resultMessage = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public event EventHandler IsResultModeMessage;
         public ResultViewModel(MainWindowViewModel? mainWindowViewModel)
         {
@@ -43,11 +56,22 @@
         {
             IsResultMode = false;
         }
-        public void EnableResultMode()
+        public void EnableResultMode(int numberOfQuestionsInPack, int correctAnswersGiven)
         {
+            NumberOfQuestionsInPack = numberOfQuestionsInPack;
+            CorrectAnswersGiven = correctAnswersGiven;
+
+            double resultPercentage = (double)correctAnswersGiven / numberOfQuestionsInPack;
+
+            ResultMessage = resultPercentage switch
+            {
+                < 0.5 => "Try a bit harder, champ",
+                < 0.8 => "Good job, you could do better though.",
+                < 1 => "Come on! Almost a perfect score!",
+                _ => "Very impressive!",
+            };
+
             IsResultModeMessage.Invoke(this, EventArgs.Empty);
-            NumberOfQuestionsInPack = mainWindowViewModel.PlayerViewModel.NumberOfQuestionsInPack;
-            CorrectAnswersGiven = mainWindowViewModel.PlayerViewModel.CorrectAnswersGiven;
             IsResultMode = true;
         }
     }
