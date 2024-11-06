fbc095e0c8fbb7896c121eb76f423936861250fa
hoarseProgramming <hampus.es@gmail.com>
2024-11-06 15:05:15 +0100

    Now getting categories, questions and HTTP decoding works

--- APIHandler.cs
+++ APIHandler.cs
@@ -1,34 +1,53 @@
-﻿using Labb3_HES.Model;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Text.Json;
+using System.Web;
 
 namespace Labb3_HES
 {
     class APIHandler
     {
+        public static async Task CheckConnectionToOpenTDB()
+        {
+            HttpClient client = new HttpClient();
+
+            var myVar = await client.GetStreamAsync("https://opentdb.com");
+        }
         public static async Task<CategoryList> GetQuestionCategories()
         {
             HttpClient client = new HttpClient();
 
-            //Asynchronous method starts
             var serializedCategories = await client.GetStringAsync("https://opentdb.com/api_category.php");
 
             var categoryList = JsonSerializer.Deserialize<CategoryList>(serializedCategories);
 
             return categoryList;
         }
-        public static async Task<List<Question>> GetQuestions()
+        public static async Task<APIQuestionRequest> GetQuestions(int amount, int categoryId, string difficulty)
         {
             HttpClient client = new HttpClient();
 
-            //Asynchronous method starts
-            var serializedCategories = await client.GetStringAsync("https://opentdb.com/api.php?amount=10");
+            string urlForAPIRequest = $"https://opentdb.com/api.php?amount={amount}&category={categoryId}&difficulty={difficulty}&type=multiple";
 
-            var categoryList = JsonSerializer.Deserialize<CategoryList>(serializedCategories);
+            var serializedQuestions = await client.GetStringAsync(urlForAPIRequest);
 
-            var questions = new List<Question>();
+            var questionRequest = JsonSerializer.Deserialize<APIQuestionRequest>(serializedQuestions);
 
-            return questions;
+            DecodeFromHTTP(questionRequest);
+
+            return questionRequest;
+        }
+        private static void DecodeFromHTTP(APIQuestionRequest questionRequest)
+        {
+            foreach (var question in questionRequest.ImportedQuestions)
+            {
+                question.Query = HttpUtility.HtmlDecode(question.Query);
+                question.CorrectAnswer = HttpUtility.HtmlDecode(question.CorrectAnswer);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    question.IncorrectAnswers[i] = HttpUtility.HtmlDecode(question.IncorrectAnswers[i]);
+                }
+            }
         }
     }
 }
