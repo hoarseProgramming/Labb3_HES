d31329c47d5f79083b1d90c6208d7adda00a8939
hoarseProgramming <hampus.es@gmail.com>
2024-11-06 15:46:05 +0100

    cleaned up code

--- Views/MenuView.xaml.cs
+++ Views/MenuView.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace Labb3_HES.Views
 {
-    /// <summary>
-    /// Interaction logic for MenuView.xaml
-    /// </summary>
     public partial class MenuView : UserControl
     {
         public MenuView()
