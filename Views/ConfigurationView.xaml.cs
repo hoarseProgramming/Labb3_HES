5eec4702d447f9f33455ceb8318a6776ce3263bd
hoarseProgramming <hampus.es@gmail.com>
2024-11-06 15:36:15 +0100

    Cleaned up code

--- Views/ConfigurationView.xaml.cs
+++ Views/ConfigurationView.xaml.cs
@@ -3,9 +3,6 @@ using System.Windows.Controls;
 
 namespace Labb3_HES.Views
 {
-    /// <summary>
-    /// Interaction logic for ConfigurationView.xaml
-    /// </summary>
     public partial class ConfigurationView : UserControl
     {
         public ConfigurationView()
