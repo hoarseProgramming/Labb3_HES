4a653219a984604eded5098ee69d1e8cd6d88f7d
hoarseProgramming <hampus.es@gmail.com>
2024-11-06 15:03:34 +0100

    update constructor to work with json and API requests.

--- Model/Question.cs
+++ Model/Question.cs
@@ -4,34 +4,12 @@ namespace Labb3_HES.Model
 {
     class Question
     {
-        public Question(string[] incorrectAnswers, string query = "New Question", string correctAnswer = "")
+        public Question(string[] incorrectAnswers, string correctAnswer, string query = "New Question")
         {
             Query = query;
             CorrectAnswer = correctAnswer;
             IncorrectAnswers = incorrectAnswers;
         }
-        //public Question(string query = "New Question", string correctAnswer = "",
-        //    string incorrectAnswer1 = "", string incorrectAnswer2 = "", string incorrectAnswer3 = "")
-        //{
-        //    Query = query;
-        //    CorrectAnswer = correctAnswer;
-        //    IncorrectAnswers = [incorrectAnswer1, incorrectAnswer2, incorrectAnswer3];
-        //}
-        //public Question(string[] incorrectAnswers, string query, string correctAnswer)
-        //{
-        //    Query = query;
-        //    CorrectAnswer = correctAnswer;
-        //    IncorrectAnswers = incorrectAnswers;
-        //}
-
-        //[JsonConstructor]
-        //public Question(string query, string correctAnswer, string[] incorrectAnswers)
-        //{
-        //    Query = query;
-        //    CorrectAnswer = correctAnswer;
-        //    IncorrectAnswers = incorrectAnswers;
-        //}
-
         [JsonIgnore]
         public string type { get; set; }
         [JsonIgnore]
@@ -40,7 +18,9 @@ namespace Labb3_HES.Model
         public string category { get; set; }
         [JsonPropertyName("question")]
         public string Query { get; set; }
+        [JsonPropertyName("correct_answer")]
         public string CorrectAnswer { get; set; }
+        [JsonPropertyName("incorrect_answers")]
         public string[] IncorrectAnswers { get; set; }
 
     }
