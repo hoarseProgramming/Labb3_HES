d75930ebf8cf91fc9645f7f87cc2e38afaa3b379
hoarseProgramming <hampus.es@gmail.com>
2024-11-07 22:41:52 +0100

    Moved these to model, felt like a good call although I'm not sure if they fit.

---
+++ Model/APIHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Web;
+
+namespace Labb3_HES.Model
+{
+    static class APIHandler
+    {
+        public static async Task<CategoryList> GetQuestionCategories()
+        {
+            HttpClient client = new HttpClient();
+
+            var serializedCategories = await client.GetStringAsync("https://opentdb.com/api_category.php");
+
+            var categoryList = JsonSerializer.Deserialize<CategoryList>(serializedCategories);
+
+            return categoryList;
+        }
+
+        public static async Task<APIQuestionRequest> GetQuestions(int amount, int categoryId, string difficulty)
+        {
+            HttpClient client = new HttpClient();
+
+            string urlForAPIRequest = $"https://opentdb.com/api.php?amount={amount}&category={categoryId}&difficulty={difficulty}&type=multiple";
+
+            var serializedQuestions = await client.GetStringAsync(urlForAPIRequest);
+
+            var questionRequest = JsonSerializer.Deserialize<APIQuestionRequest>(serializedQuestions);
+
+            DecodeFromHTTP(questionRequest);
+
+            return questionRequest;
+        }
+
+        private static void DecodeFromHTTP(APIQuestionRequest questionRequest)
+        {
+            foreach (var question in questionRequest.ImportedQuestions)
+            {
+                question.Query = HttpUtility.HtmlDecode(question.Query);
+                question.CorrectAnswer = HttpUtility.HtmlDecode(question.CorrectAnswer);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    question.IncorrectAnswers[i] = HttpUtility.HtmlDecode(question.IncorrectAnswers[i]);
+                }
+            }
+        }
+    }
+}
