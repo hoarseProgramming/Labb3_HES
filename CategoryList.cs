005de8f13cf32ddee74415188cc7425994acbf3a
hoarseProgramming <hampus.es@gmail.com>
2024-11-07 13:15:19 +0100

    Small code clean up

--- CategoryList.cs
+++ CategoryList.cs
@@ -3,10 +3,10 @@
 namespace Labb3_HES
 {
     class CategoryList
-
     {
         [JsonPropertyName("trivia_categories")]
         public List<Category> ListOfCategories { get; }
+
         public CategoryList(List<Category> listOfCategories)
         {
             this.ListOfCategories = listOfCategories;
