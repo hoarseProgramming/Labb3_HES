d02cf4184c64cd58dbca789fb57856a8776609bb
hoarseProgramming <hampus.es@gmail.com>
2024-11-07 21:50:17 +0100

    Add dialog for unplayable pack

---
+++ Dialogs/ActivePackNotPlayableDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Labb3_HES.Dialogs
+{
+    public partial class ActivePackNotPlayableDialog : Window
+    {
+        public ActivePackNotPlayableDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
