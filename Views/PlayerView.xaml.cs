906cc0eca4dfff1215c7980b70023ee2e5843c05
hoarseProgramming <hampus.es@gmail.com>
2024-11-09 00:27:49 +0100

    Removed all code behind eventlisteners

--- Views/PlayerView.xaml.cs
+++ Views/PlayerView.xaml.cs
@@ -1,62 +1,12 @@
-﻿using Labb3_HES.ViewModel;
-using System.Windows.Controls;
-using System.Windows.Media;
+﻿using System.Windows.Controls;
 
 namespace Labb3_HES.Views
 {
     public partial class PlayerView : UserControl
     {
-        private List<Button> buttons = new();
-        private Color colorAliceBlue = new() { R = 240, G = 248, B = 255, A = 255 };
-        private Color colorGreen = new() { G = 255, A = 255 };
-        private Color colorRed = new() { R = 255, A = 255 };
-
         public PlayerView()
         {
             InitializeComponent();
-            buttons.Add(buttonOne);
-            buttons.Add(buttonTwo);
-            buttons.Add(buttonThree);
-            buttons.Add(buttonFour);
-        }
-        public void OnAnswerRecievedMessageRecieved(object sender, EventArgs args)
-        {
-            foreach (Button button in buttons)
-            {
-                string answerInButton = (button.Content as TextBlock).Text;
-                bool answerInButtonIsCorrectAnswer = answerInButton == (DataContext as PlayerViewModel).ActiveQuestion.CorrectAnswer;
-                bool answerInButtonIsGivenAnswer = answerInButton == (DataContext as PlayerViewModel).GivenAnswer;
-
-                if (answerInButtonIsCorrectAnswer)
-                {
-                    button.Background = new SolidColorBrush(colorGreen);
-                }
-                else if (answerInButtonIsGivenAnswer && !answerInButtonIsCorrectAnswer)
-                {
-                    button.Background = new SolidColorBrush(colorRed);
-                }
-            }
-        }
-
-        public void OnNoAnswerRecievedMessageRecieved(object sender, EventArgs args)
-        {
-            foreach (Button button in buttons)
-            {
-                string answerInButton = (button.Content as TextBlock).Text;
-                bool answerInButtonIsCorrectAnswer = answerInButton == (DataContext as PlayerViewModel).ActiveQuestion.CorrectAnswer;
-
-                if (answerInButtonIsCorrectAnswer)
-                {
-                    button.Background = new SolidColorBrush(colorGreen);
-                }
-            }
-        }
-        public void OnIsNewQuestionMessageRecieved(object sender, EventArgs args)
-        {
-            foreach (Button button in buttons)
-            {
-                button.Background = new SolidColorBrush(colorAliceBlue);
-            }
         }
     }
 }
